c8c1abdbbd37d3839c41981092aac2c33cf59b93
Katherine Hughes <kjh53@student.le.ac.uk>
2022-02-01 16:35:55 +0000

    resolved merge conflict

--- spicesrus/src/main/java/com/example/repo/ProductRepository.java
+++ spicesrus/src/main/java/com/example/repo/ProductRepository.java
@@ -1,17 +1,9 @@
-<<<<<<< HEAD:y2 project/src/main/java/spiceshop/y2project/repo/ProductRepository.java
-package spiceshop.y2project.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import spiceshop.y2project.domain.Product;
-=======
 package com.example.repo;
 
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.domain.Product;
 
->>>>>>> draft1:spicesrus/src/main/java/com/example/repo/ProductRepository.java
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
