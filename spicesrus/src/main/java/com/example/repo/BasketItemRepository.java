8bc6c90a3cbfdecb5e6c52f94a76aea990c3f888
“M A A Syed <maas6@student.le.ac.uk>
2022-02-23 07:39:39 +0000

    Basket and Items Repo

---
+++ spicesrus/src/main/java/com/example/repo/BasketItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.domain.BasketItem;
+
+public interface BasketItemRepository extends CrudRepository<BasketItem, Integer>{
+
+}
